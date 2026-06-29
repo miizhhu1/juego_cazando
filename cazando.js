9fd78b856e2f6b4971ea556520243ded86baecc2
Miizhhu1 <miizhhu@gmail.com>
2026-06-28 20:37:21 -0500

    crear graficarRectangulo  y modificar graficarGato y graficarComida

--- cazando.js
+++ cazando.js
@@ -11,18 +11,18 @@ let gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
 let comidaX = canvas.width - ANCHO_COMIDA;;
 let comidaY = canvas.height - ALTO_COMIDA;
 
-
-
-// 4. Dibujar el rectángulo
 function graficarGato(){
-    ctx.fillStyle="#f47fb9";
-    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+
+    graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, '#f47fb9'); 
 }
 
-//5. Dibujar el cuadrado
 function graficarComida(){
-    ctx.fillStyle="#7fc9f4";
-    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
+    graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, '#7fc9f4');
+}
+
+function graficarRectangulo (x, y, alto, ancho, color){
+    ctx.fillStyle=color;
+    ctx.fillRect(x, y, alto, ancho);
 }
 
 function iniciarJuego(){
