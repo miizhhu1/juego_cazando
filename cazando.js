28c85c7a56d5c53ba5367443bd9ffa27104a877d
Miizhhu1 <miizhhu@gmail.com>
2026-07-07 21:56:11 -0500

    correciones juego cazando

--- cazando.js
+++ cazando.js
@@ -15,13 +15,26 @@ let tiempo=10;
 let intervaloTiempo=1000;
 let intervalo=0;
 
-function graficarGato(){
+function iniciarJuego(){
+    gatoX = (canvas.width - ANCHO_GATO) / 2;     
+    gatoY = (canvas.height - ALTO_GATO) / 2;
+    aparecerComida();
+    limpiarCanva();
+    graficarGato();
+    graficarComida();
+    intervalo=setInterval(restarTiempo,intervaloTiempo); 
+}
+function aparecerComida(){
+    comidaX=generarAleatorio(0,canvas.width-ALTO_COMIDA);
+    comidaY=generarAleatorio(0,canvas.height-ANCHO_COMIDA);
+}
 
-    graficarRectangulo((canvas.width - ANCHO_GATO) / 2, (canvas.height - ALTO_GATO) / 2, ANCHO_GATO, ALTO_GATO, '#f47fb9'); 
+function graficarGato(){
+    graficarRectangulo(gatoX, gatoY, ANCHO_GATO, ALTO_GATO, '#f47fb9'); 
 }
 
 function graficarComida(){
-    graficarRectangulo(canvas.width - ANCHO_COMIDA, canvas.height - ALTO_COMIDA, ANCHO_COMIDA, ALTO_COMIDA, '#7fc9f4');
+    graficarRectangulo(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA, '#7fc9f4');
 }
 
 function graficarRectangulo (x, y, ancho, alto, color){
@@ -29,73 +42,60 @@ function graficarRectangulo (x, y, ancho, alto, color){
     ctx.fillRect(x, y, ancho, alto);
 }
 
-function iniciarJuego(){
-    graficarGato();
-    graficarComida();
-    intervalo=setInterval(restarTiempo,intervaloTiempo); 
-    
-}
-
-
-
-function moverIzquierda(){
-    gatoX=gatoX-10;
-    limpiarCanva();
-    graficarGato();
-    graficarComida();
-    detectarColision();
-}
-function moverDerecha(){
-    gatoX=gatoX+10;
-    limpiarCanva();
-    graficarGato();
-    graficarComida();
-    detectarColision();
-}
-function moverArriba(){
-    gatoY=gatoY-10;
-    limpiarCanva();
-    graficarGato();
-    graficarComida();
-    detectarColision();
-}
-function moverAbajo(){
-    gatoY=gatoY+10;
-    limpiarCanva();
-    graficarGato();
-    graficarComida();
-    detectarColision();
-}
 function limpiarCanva(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);   
 }
+function moverIzquierda(){
+    gatoX=gatoX-10;
+    actualizarPantalla();
+}
+function moverDerecha(){
+    gatoX=gatoX+10;
+    actualizarPantalla();
+}
+function moverArriba(){
+    gatoY=gatoY-10;
+    actualizarPantalla();
+}
+function moverAbajo(){
+    gatoY=gatoY+10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    limpiarCanva();
+    graficarGato();
+    graficarComida();
+    detectarColision();
+}
+
 
 function detectarColision(){
     if(gatoX+ANCHO_GATO>comidaX &&
-        gatoX<comidaX+ANCHO_COMIDA && gatoY+ALTO_GATO>comidaY &&
+        gatoX<comidaX+ANCHO_COMIDA && 
+        gatoY+ALTO_GATO>comidaY &&
         gatoY<comidaY+ALTO_COMIDA){
             //alert("atrapado"); 
-            aparecerComida();
             puntaje=puntaje+1;
             mostrarEnSpan("puntos",puntaje);
+            aparecerComida();
+            limpiarCanva();
+            graficarGato();
+            graficarComida();            
         }
-    if(puntaje===6){
+    if(puntaje===2){
         alert("ERES EL GANADOR, GENIAL :D");      
         clearInterval(intervalo); // detenemos el setInterval
-
     }
     
 }
 
-function aparecerComida(){
-    comidaX=generarAleatorio(0,canvas.width-ALTO_COMIDA);
-    comidaY=generarAleatorio(0,canvas.height-ANCHO_COMIDA);
-}
+
 
 function restarTiempo(){
     tiempo=tiempo-1;
     mostrarEnSpan("tiempo",tiempo); 
-    detectarColision(); 
+    
     if(tiempo===0){
         alert("GAME OVER :/");      
         clearInterval(intervalo); // detenemos el setInterval
@@ -114,6 +114,9 @@ function reiniciar(){
     //limpiamos cualquier intervalo viejo por seguridad antes de arrancar
     clearInterval(intervalo);
     iniciarJuego(); // invocamos a la funcion iniciar
-
+    
    
 }
+
+
+
